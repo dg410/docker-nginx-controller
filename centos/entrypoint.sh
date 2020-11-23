4cfa55467967b1dc6c879e555fb8bed14d4e2297
root <root@api-gw-image.med.one>
2020-11-23 15:12:11 +0200

    nginx and waf

--- centos/entrypoint.sh
+++ centos/entrypoint.sh
@@ -90,6 +90,10 @@ fi
 
 echo "starting controller-agent ..."
 /usr/bin/nginx-controller-agent > /dev/null 2>&1 < /dev/null &
+/bin/su -s /bin/bash -c '/opt/app_protect/bin/bd_agent &' nginx
+/bin/su -s /bin/bash -c "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 307200000 total_umu_max_size 3129344 sys_max_account_id 1024 \
+no_static_config 2>&1 > /var/log/app_protect/bd-socket-plugin.log &" nginx
+/usr/sbin/nginx -g 'daemon off;'
 
 agent_pid=$!
 
